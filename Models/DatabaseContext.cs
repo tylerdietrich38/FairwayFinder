e9f50491102d0a15386b01b280f6e88ad8e35213
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-06-28 13:17:09 -0400

    prevent duplicate users

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -18,6 +18,10 @@ namespace FairwayFinder.Models
         // Tell the context about the User collection/table
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(user => user.Email).IsUnique();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
