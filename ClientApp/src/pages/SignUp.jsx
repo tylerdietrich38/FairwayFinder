3f0c0131bcd1f2ef6b6226ae80460a41f2f69913
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-06-29 10:33:26 -0400

    isLoggedIn, failed to compile

--- ClientApp/src/pages/SignUp.jsx
+++ ClientApp/src/pages/SignUp.jsx
@@ -2,10 +2,47 @@ import React, { useState } from 'react'
 import logo from '../Images/Logo.png'
 import { Link } from 'react-router-dom'
 import { useHistory } from 'react-router-dom'
+import { recordAuthentication } from '../auth'
+import { isLoggedIn } from './auth'
 
 export function SignUp() {
   const history = useHistory()
   const [errorMessage, setErrorMessage] = useState()
+
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+
+  function handleStringFieldChange(event) {
+    const value = event.target.value
+    const fieldName = event.target.name
+
+    const updatedUser = { ...user, [fieldName]: value }
+
+    setUser(updatedUser)
+  }
+
+  async function handleFormSubmit(event) {
+    event.preventDefault()
+
+    const response = await fetch('/api/Sessions', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(user),
+    })
+
+    const apiResponse = await response.json()
+
+    if (apiResponse.status === 400) {
+      setErrorMessage(Object.values(apiResponse.errors).join(' '))
+    } else {
+      // TODO, record the login
+      // recordAuthentication(apiResponse)
+      recordAuthentication(apiResponse)
+      window.location.assign('/')
+    }
+  }
 
   const [newUser, setNewUser] = useState({
     fullName: '',
@@ -56,9 +93,11 @@ export function SignUp() {
                 <li>Golf Courses</li>
               </Link>
               <li>|</li>
-              <Link to="/signin">
-                <li>Sign In/Sign Up</li>
-              </Link>
+              {isLoggedIn() ? null : (
+                <Link to="/signin">
+                  <li>Sign In/Sign Up</li>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
@@ -68,14 +107,25 @@ export function SignUp() {
               <i className="fa fa-home"></i>
             </a>
             <h2>Sign In</h2>
-            <form action="#">
+            <form onSubmit={handleFormSubmit}>
+              {errorMessage ? <p>{errorMessage}</p> : null}
               <p className="form-input">
                 <label htmlFor="name">Full Name</label>
-                <input type="text" name="fullName" />
+                <input
+                  type="text"
+                  name="fullName"
+                  value={user.fullName}
+                  onChange={handleStringFieldChange}
+                />
               </p>
               <p className="form-input">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" />
+                <input
+                  type="password"
+                  name="password"
+                  value={user.password}
+                  onChange={handleStringFieldChange}
+                />
               </p>
               <p>
                 <input type="submit" value="Sign In" />
